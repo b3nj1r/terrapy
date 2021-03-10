e37d2556436f4d2ab9b9352d95af1ad5e7552e0e
b3nj1r <47711426+b3nj1r@users.noreply.github.com>
2021-03-10 07:37:27 -0800

    organized coordinate pairs and temporarily visualized with matplotlib

--- src/data.py
+++ src/data.py
@@ -1,10 +1,9 @@
-#request and format data
-import matplotlib.pyplot as plt
 import numpy as np
 import requests
 
 # request height data from opentopodata
 loc= '32,-97 | 32.1, -97 | 32.2, -97 | 32.3, -97 | 32.4, -97 | 32.5, -97 | 32.6, -97 | 32.7, -97 | 32.8, -97 | 32.9, -97 | 33, -97'
+
 params = {'locations':loc,'interpolation':'cubic'}
 url = 'https://api.opentopodata.org/v1/ned10m'
 r = requests.get(url,params=params)
@@ -13,8 +12,9 @@ r = requests.get(url,params=params)
 pmap = r.json()['results']
 
 # assign height values to coordinate pairs
-ps = []
+x,y,z = [],[],[]
 for p in pmap:
-    ps.append([p['location']['lat'],p["location"]['lng'],p['elevation']])
+    x.append(p['location']['lat'])
+    y.append(p["location"]['lng'])
+    z.append(p['elevation'])
 
-print(ps)
--- src/process.py
+++ src/process.py
@@ -1,1 +1,12 @@
-# compute requested data into mesh object
+import data
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+x,y,z = data.x,data.y,data.z
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(x,y,z)
+plt.show()
+plt.savefig("../out/fig.png")
+
